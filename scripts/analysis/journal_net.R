16f5e47322fe73d4dddad22f3e7b36fa901c5035
TimothyElder <timothyelder@uchicago.edu>
2021-10-12 18:06:41 -0500

    journal to journal network code working

--- scripts/analysis/journal_net.R
+++ scripts/analysis/journal_net.R
@@ -1,12 +1,19 @@
 library(tidyverse)
-library(statnet)
+library(network)
 
-setwd("/Users/timothyelder/Documents/mag")
+setwd("/home/timothyelder/mag")
 
 journal2journal <- read.table("data/journal2journal_mat.txt")
 
 sum(journal2journal)
 
-journal_net <- as.network(journal2journal, directed = FALSE, bipartite = FALSE, loops = TRUE, multiple = TRUE)
+# removing x from column names so the matrix is totally symmetric
+colnames(journal2journal) <- sub("X", "", colnames(journal2journal))
+
+journal2journal <- as.matrix(journal2journal)
+
+journal_net <- as.network(journal2journal, matrix.type = "adjacency", ignore.eval = FALSE, names.eval = "weight", loops = TRUE)
 
 rm(journal2journal)
+
+network.density(journal_net)
