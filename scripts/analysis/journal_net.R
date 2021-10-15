ab556c60b8e97156c65f825b2de072b57840224b
TimothyElder <TimothyElder@uchicago.edu>
2021-10-15 18:18:57 -0500

    analysis of the edge list generated from gen_edgelist.py, need to review columns and visualize data

--- scripts/analysis/journal_net.R
+++ scripts/analysis/journal_net.R
@@ -2,9 +2,17 @@ library(tidyverse)
 library(network)
 library(sna)
 
-setwd("/home/timothyelder/mag")
+#setwd("/home/timothyelder/mag")
 
 journal2journal <- read.table("data/journal2journal_mat.txt")
+
+papers2journals <- read.csv("data/edge_list.csv")
+
+authors <- read.csv("data/authors.csv")
+authors2journals <- read.csv("data/authors2journals.csv")
+
+authors <- authors %>% 
+  select(network_name, AuthorId)
 
 df <- read.csv('data/journals.csv')
 df <- df[,2:3]
@@ -58,3 +66,46 @@ new_df <- data.frame(match, row.names(journal2journal))
 match <- journal2journal$`3880285`
 
 new_df <- data.frame(match, row.names(journal2journal))
+
+#### Calcualting mean eigen scores for authors #####
+
+another_df <- merge(papers2journals, journal_df, by = "JournalId")
+
+authors_df <- merge(another_df, authors, by = "AuthorId")
+
+
+authors_df <- authors_df %>% 
+  group_by(network_name) %>%
+  summarise(eigen_mean = mean(eigen), prank_mean = mean(prank.vector), n = n(), degree_mean = mean(degree))
+  
+eig <- ggplot(authors_df, aes(x = eigen_mean)) + geom_density() + ggtitle("Author Mean Eigen")
+
+prank <- ggplot(authors_df, aes(x = prank_mean)) + geom_density() + ggtitle("Authors Mean Page Rank")
+
+deg <- ggplot(authors_df, aes(x = degree_mean)) + geom_density() + ggtitle("Author Mean Degree")
+
+articles <- ggplot(authors_df, aes(x = n)) + geom_density() + ggtitle("Author NUmber of Articles")
+
+all.g <- ggpubr::ggarrange(eig, prank, deg, articles)
+
+ggsave("author_scores.pdf", all.g)
+
+#### HISTOGRAMS #####
+
+eig <- ggplot(authors_df, aes(x = eigen_mean)) + geom_histogram() + ggtitle("Author Mean Eigen")
+
+prank <- ggplot(authors_df, aes(x = prank_mean)) + geom_histogram() + ggtitle("Authors Mean Page Rank")
+
+deg <- ggplot(authors_df, aes(x = degree_mean)) + geom_histogram() + ggtitle("Author Mean Degree")
+
+articles <- ggplot(authors_df, aes(x = n)) + geom_histogram() + ggtitle("Author NUmber of Articles")
+
+all.g <- ggpubr::ggarrange(eig, prank, deg, articles)
+
+ggsave("author_scores_hist.pdf", all.g)
+
+ggplot(journal_df, aes(x = eigen)) + geom_density()
+
+ggplot(journal_df, aes(x = prank.vector)) + geom_density()
+
+ggplot(journal_df, aes(x = degree)) + geom_density()
